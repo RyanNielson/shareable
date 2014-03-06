bec0b6141b96a9af2ae4759b85ce697e68365a54
Ryan Nielson <ryan.nielson@gmail.com>
2014-03-06 19:05:58 +0000

    Simplified method

--- src/RyanNielson/Shareable/Shareable.php
+++ src/RyanNielson/Shareable/Shareable.php
@@ -8,19 +8,10 @@ class Shareable {
   public function all()
   {
     $defaultButtons = Config::get('shareable::default_buttons', array());
-    $buttonContent = '';
-    $buttons = array();
-    foreach ($defaultButtons as $button) {
-      $className = "\\RyanNielson\\Shareable\\Buttons\\" . studly_case($button);
-      if (class_exists($className))
-        $buttons[] = new $className();
-    }
-    
-    foreach ($buttons as $button) {
-      $buttonContent .= $button->render(); 
-    }
 
-    return $buttonContent; 
+    foreach ($defaultButtons as $button) {
+      call_user_func(array($this, $button));
+    }
   }
 
   public function __call($name, $arguments)
