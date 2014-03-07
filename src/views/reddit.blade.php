a899ff8c9ceb242a987e76ddd76f96458e0efc56
Ryan Nielson <ryan.nielson@gmail.com>
2014-03-07 14:11:25 +0000

    Create rough reddit button

---
+++ src/views/reddit.blade.php
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+    reddit_url = "{{{ $options['url'] }}}";
+    reddit_title = "{{{ $options['title'] }}}";
+    reddit_bgcolor = "{{{ $options['bgcolor'] }}}";
+    reddit_bordercolor = "{{{ $options['bordercolor'] }}}";
+</script>
+<script type="text/javascript" src="http://www.reddit.com/static/button/button1.js"></script>
